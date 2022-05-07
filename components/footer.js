f40be4f6f7174ce65a3967fa951affe47ce2b87d
issa <missa355@hotmail.com>
2022-05-07 15:45:57 -0400

    First satge of portfolio

---
+++ components/footer.js
@@ -0,0 +1,11 @@
+import { Box } from '@chakra-ui/react'
+
+const Footer = () => {
+  return (
+    <Box align="center" opacity={0.4} fontSize="sm">
+      {new Date().getFullYear()} Mohamed Issa
+    </Box>
+  )
+}
+
+export default Footer
---
+++ pages/index.js
@@ -0,0 +1,180 @@
+import NextLink from 'next/link'
+import {
+  Link,
+  Container,
+  Heading,
+  Box,
+  SimpleGrid,
+  Button,
+  List,
+  ListItem,
+  useColorModeValue,
+  chakra
+} from '@chakra-ui/react'
+import { ChevronRightIcon } from '@chakra-ui/icons'
+import Paragraph from '../components/paragraph'
+import { BioSection, BioYear } from '../components/bio'
+import Layout from '../components/layouts/article'
+import Section from '../components/section'
+import { GridItem } from '../components/grid-item'
+import { IoLogoTwitter, IoLogoInstagram, IoLogoGithub, IoLogoLinkedin } from 'react-icons/io5'
+import thumbYouTube from '../public/images/links/youtube.png'
+import thumbStudytogether from '../public/images/works/inkdrop_eyecatch.png'
+import Image from 'next/image'
+
+const ProfileImage = chakra(Image, {
+  shouldForwardProp: prop => ['width', 'height', 'src', 'alt'].includes(prop)
+})
+
+const Home = () => (
+  <Layout>
+    <Container>
+      <Box
+        borderRadius="lg"
+        mb={6}
+        p={3}
+        textAlign="center"
+        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+        css={{ backdropFilter: 'blur(10px)' }}
+      >
+        Hello, I&apos;m a full-stack developer from UOFT!
+      </Box>
+
+      <Box display={{ md: 'flex' }}>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant="page-title">
+            Mohamed Issa
+          </Heading>
+          <p>Digital blacksmith ( Tinker / Developer / Designer )</p>
+        </Box>
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          textAlign="center"
+        >
+          <Box
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            w="100px"
+            h="100px"
+            display="inline-block"
+            borderRadius="full"
+            overflow="hidden"
+          >
+            <ProfileImage
+              src="/images/issa.jpg"
+              alt="Profile image"
+              borderRadius="full"
+              width="100%"
+              height="100%"
+            />
+          </Box>
+        </Box>
+      </Box>
+
+      <Section delay={0.1}>
+        <Heading as="h3" variant="section-title">
+          Work
+        </Heading>
+        <Paragraph>
+          Mohamed is a full-stack developer with professional experience in FinTech industry. He has
+          a passion for building whatever digital project I deem <b>awesome</b>. He loves spend good
+          amount of his time learning different technologies and joining hackathons. He is currenlty 
+          learning AI, web3, cybersecurity and AR/VR. Outside of work, he loves working on his
+          respberry pi and arduino to see what cool things he can build.{' '}
+        </Paragraph>
+        <Box align="center" my={4}>
+          <NextLink href="/works" scroll={false}>
+            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+              My portfolio
+            </Button>
+          </NextLink>
+        </Box>
+      </Section>
+
+      <Section delay={0.2}>
+        <Heading as="h3" variant="section-title">
+          Bio
+        </Heading>
+        <BioSection>
+          <BioYear>2020</BioYear>
+          Worked at <b>Harmonic Fund Services</b> for 1.2 years
+        </BioSection>
+        <BioSection>
+          <BioYear>2022</BioYear>
+          Graduate with a Bachelor's degree. Major in Computer Science and Mathematics at <b>University of
+          Toronto</b>. 
+        </BioSection>
+      </Section>
+
+      <Section delay={0.3}>
+        <Heading as="h3" variant="section-title">
+          I ♥
+        </Heading>
+        <Paragraph>
+          Soccer, Formula 1, bike riding, raspberryPI, Arduino. 
+        </Paragraph>
+      </Section>
+
+      <Section delay={0.3}>
+        <Heading as="h3" variant="section-title">
+          On the web
+        </Heading>
+        <List>
+          <ListItem>
+            <Link href="https://github.com/missa355" target="_blank">
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                leftIcon={<IoLogoGithub />}
+              >
+                @missa355
+              </Button>
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link href="https://linkedin.com/in/mohamed-issa-257137127" target="_blank">
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                leftIcon={<IoLogoLinkedin />}
+              >
+                @Mohamed Issa
+              </Button>
+            </Link>
+          </ListItem>
+        </List>
+
+        {/* <SimpleGrid columns={[1, 2, 2]} gap={6}>
+          <GridItem
+            href="https://www.youtube.com/devaslife"
+            title="Dev as Life"
+            thumbnail={thumbYouTube}
+          >
+            My YouTube channel
+          </GridItem>
+          <GridItem
+            href="https://www.inkdrop.app/"
+            title="Inkdrop"
+            thumbnail={thumbInkdrop}
+          >
+            A Markdown note-taking app
+          </GridItem>
+        </SimpleGrid>
+
+        <Box align="center" my={4}>
+          <NextLink href="/posts" scroll={false}>
+            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+              Popular posts
+            </Button>
+          </NextLink>
+        </Box> */}
+      </Section>
+    </Container>
+  </Layout>
+)
+
+export default Home
+export { getServerSideProps } from '../components/chakra'
